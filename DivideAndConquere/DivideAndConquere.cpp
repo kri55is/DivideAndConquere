72518466f0a696588397f9641e5632d4d8a6df68
kri55is <brisseau.emilie@gmail.com>
2015-05-29 15:36:21 -0700

    divide and conquere : sort array of integer

---
+++ DivideAndConquere/DivideAndConquere.cpp
@@ -0,0 +1,74 @@
+#include <math.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printVect(vector<int> v){
+	int size = v.size();
+	for (int i = 0; i < size; i++){
+		cout << v.at(i) << " ";
+	}
+}
+
+vector<int> DivideAndConquere(vector<int> v){
+	int size = v.size();
+	int half_size = size / 2;
+	if (size < 1){
+		cout << "Error size is " << size << endl;
+		return  v;
+	}
+	if (size == 1) {
+		cout << "out vector " << v[0] << endl;
+		return v;
+	}
+
+	printVect(v); cout << "--->";
+	vector<int> v_out(size);
+	vector<int> v_left(v.begin(), v.begin() + size - half_size);
+	printVect(v_left); cout << "----";
+	vector<int> v_right(v.begin() + size - half_size, v.end());
+	printVect(v_right); cout << endl;
+
+	v_left = DivideAndConquere(v_left);
+	v_right = DivideAndConquere(v_right);
+
+	//merge
+	int left = 0;
+	int right = 0;
+	for (int i = 0; i < size; i++){
+		if ((left >= size - half_size) || (v_right[right] < v_left[left])){
+			v_out[i] = v_right[right];
+			right++;
+		}
+		else{
+			if ((right >= half_size) || (v_left[left] < v_right[right])){
+				v_out[i] = v_left[left];
+				left++;
+			}
+		}
+	}
+	cout << "out vector ";
+	printVect(v_out); cout << endl;
+
+	return v_out;
+}
+
+void DivideAndConquereAndTest(){
+	vector <int> v = { 38, 27, 43, 3, 9, 82, 10 };
+	cout << endl << "un-ordered vector ";
+	printVect(v); cout << endl;
+
+	v = DivideAndConquere(v);
+	cout << endl << "ordered vector ";
+	printVect(v); cout << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	//KaratsubaMultiplicationAndTest();
+	DivideAndConquereAndTest();
+	system("pause");
+	return 0;
+}
+
